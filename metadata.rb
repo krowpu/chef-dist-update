b6c2230082d1caa75f237e301cba136b000b2ca3
krowpu <krowpu@tightmail.com>
2017-03-19 14:08:02 +0500

    Add metadata

--- metadata.rb
+++ metadata.rb
@@ -2,4 +2,10 @@ name 'dist-update'
 description 'Retrieve new lists of system packages'
 license 'MIT'
 
+maintainer 'krowpu'
+maintainer_email 'krowpu@tightmail.com'
+
+source_url 'https://github.com/krowpu/chef-dist-update'
+issues_url 'https://github.com/krowpu/chef-dist-update/issues'
+
 platform 'ubuntu', '= 16.04'
